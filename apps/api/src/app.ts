5af14cf14d24a5b3c8e108280b249e0621499aa6
mfnahdi <mfnahdi8@gmail.com>
2024-01-29 06:06:30 +0700

    feat: all in

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -8,7 +8,9 @@ import express, {
 } from 'express';
 import cors from 'cors';
 import { PORT } from './config';
-import { SampleRouter } from './routers/sample.router';
+
+import { EventRouter } from './routers/eventRoutes';
+import { UserRouter } from './routers/user.router';
 
 export default class App {
   private app: Express;
@@ -24,6 +26,7 @@ export default class App {
     this.app.use(cors());
     this.app.use(json());
     this.app.use(urlencoded({ extended: true }));
+    this.app.use('/api/images', express.static('images'));
   }
 
   private handleError(): void {
@@ -50,18 +53,20 @@ export default class App {
   }
 
   private routes(): void {
-    const sampleRouter = new SampleRouter();
+    // const userRouter = new UserRouter();
+    const eventsRouter = new EventRouter();
 
     this.app.get('/api', (req: Request, res: Response) => {
       res.send(`Hello, Purwadhika Student !`);
     });
 
-    this.app.use('/api/samples', sampleRouter.getRouter());
+    // this.app.use('/api/users', userRouter.getRouter());
+    this.app.use('/api/events', eventsRouter.getRouter());
   }
 
   public start(): void {
     this.app.listen(PORT, () => {
-      console.log(`  ➜  [API] Local:   http://localhost:${PORT}/`);
+      console.log(`  ➜  [API] Local: http://localhost:${PORT}/`);
     });
   }
 }
---
+++ apps/api/src/routers/eventRoutes.ts
@@ -0,0 +1,26 @@
+import { EventController } from '@/controllers/eventController';
+import { Router } from 'express';
+
+export class EventRouter {
+  private router: Router;
+  private eventController: EventController;
+  constructor() {
+    this.eventController = new EventController();
+    this.router = Router();
+    this.initializeRoutes();
+  }
+  private initializeRoutes(): void {
+    this.router.get('/', this.eventController.getEvents);
+    this.router.post('/', this.eventController.createEvent);
+    this.router.get('/:id', this.eventController.getEventById);
+    this.router.patch('/:id', this.eventController.updateEvent);
+
+    this.router.get(
+      '/filter/category',
+      this.eventController.getEventsByCategory,
+    );
+  }
+  getRouter(): Router {
+    return this.router;
+  }
+}
